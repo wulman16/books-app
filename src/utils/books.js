8da3350d95a238b2e0c7e57c3294f34792a763ef
wulman16 <will.ulman@gmail.com>
2019-06-02 11:21:54 -0400

    add error handling with API calls

--- src/utils/books.js
+++ src/utils/books.js
@@ -4,12 +4,16 @@ const books = (search, callback) => {
   const searchTerm = encodeURIComponent(search);
   const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&maxResults=10`;
   request({ url: url, json: true }, (error, { body }) => {
-    if (error) {
-      callback(error);
+    if (body.error) {
+      callback(body.error.message);
+    } else if (body.totalItems === 0) {
+      callback(
+        `No volumes found! Please change your search terms and try again.`
+      );
     } else {
       callback(body.items);
     }
   });
 };
 
-books(``, console.log);
+books(`skjvbhr784hfwiurf`, console.log);
